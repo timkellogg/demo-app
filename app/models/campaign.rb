3cd550474fd584c0303c39d33df1d924d2a990d6
timkellogg <tim.kellogg@gmail.com>
2016-02-03 20:09:03 -0800

    improve method to handle files

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,15 +1,30 @@
+require 'roo'
+
 class Campaign < ActiveRecord::Base
   belongs_to :company
   
   # converts file uploads into campaign model
   def self.import(file)
-    spreadsheet = open_spreadsheet(file)
     
-    header = spreadsheet.row(1)
-    (2..spreadsheet.last_row).each do |i|
-      row = Hash[[header, spreadsheet.row(i)].transpose]
-      campaign = find_by_id(row['id']) || new
-      campaign.attributes = row.to_hash.slice(*accessible_attributes)
+    # TODO: Method to detect file extension and parse csv,xls,libre differently
+    # TODO: Fix parsing of file
+    begin 
+      workbook = Roo::Excelx.new(file.path)
+      workbook.default_sheet = workbook.sheets.first
+      hash_of_campaign_attributes = ((workbook.first_row + 1)..workbook.last_row).each_with_object({}) do |row, hash|
+        next if workbook.row(row).nil?
+          company_id = workbook.row(row)[0]
+          name = workbook.row(row)[1]
+          channel = workbook.row(row)[2]
+          campaign_type = workbook.row(row)[3]
+          campaign_date = workbook.row(row)[4]
+      end
+      
+      puts "#{hash_of_campaign_attributes}"
+      
+    rescue => e
+      Rails.logger << "Error while processing file upload"
+      # TODO: add file upload error to display on page to user
     end
   end
   
@@ -20,6 +35,5 @@ class Campaign < ActiveRecord::Base
                         :name,
                         :channel,
                         :campaign_type,
-                        :campaign_date,
-                        :file_url
+                        :campaign_date
 end
